dabda1fec4b2d0e9e00479ff3afaa206b3f307ea
MattMX <mangrovethefox@outlook.com>
2024-05-29 20:13:08 +0100

    light mode

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 let projects
 
 $(() => {
-    if (window.location.hash) {
-        refreshSections()
-    }
+    refreshSections()
+    setColourScheme()
 
     setTimeout(() => {
         $("#loading-overlay")
@@ -14,17 +13,29 @@ $(() => {
     }, 4)
 
     $("#modal-container").on("click", function (event) {
-        $(this).hide()
+        if (event.target == $(this).get(0)) {
+            $(this).hide()
+        }
     })
 
-    const props = {
+    $("#toggle-theme").on("click", function (event) {
+        const newTheme = document.documentElement.getAttribute("data-theme") == "dark"
+            ? "light" : "dark"
+
+        document.documentElement.setAttribute("data-theme", newTheme)
+        localStorage.setItem("theme", newTheme)
+        
+        updateThemeButton(newTheme)
+    })
+
+    const strProps = {
         age: Math.abs(new Date(Date.now() - new Date('28 Nov 2003 00:00:00 GMT')).getFullYear() - 1970)
     }
 
-    $("b").each(function() {
+    $("b").each(function () {
         let text = $(this).text();
-        for (let prop of Object.keys(props)) {
-            text = text.replaceAll(`{{${prop}}}`, props[prop])
+        for (let prop of Object.keys(strProps)) {
+            text = text.replaceAll(`{{${prop}}}`, strProps[prop])
         }
         $(this).text(text)
     })
@@ -46,25 +57,26 @@ $(() => {
         })
 })
 
+function hideModal() {
+    $("#modal-container").hide()
+}
+
 function showProjectModal(id) {
     const project = projects.find((p) => p.id == id);
     $("#modal-container").css({
         display: "flex",
-        opacity: 0,
-        transform: `scale(0)`
+        opacity: 0
     })
 
     $("#modal-container").animate({
         opacity: 1,
-    }, {
-        step: function(now, fx) {
-            $(this).css('transform', 'scale(' + now + ')')
-        },
-        duration: 200
-    })
+    }, 200)
 
     $("#modal-container").html(`
         <div class="modal">
+            <button id="close-modal" onclick='hideModal()'>
+                <i class="fa fa-close"></i>
+            </button>
                 <div class="thumb">
                     <img src="${project.thumb || "/assets/thumb.png"}">
                     <div class="overlay"></div>
@@ -95,4 +107,25 @@ function refreshSections() {
 
 function toggleResponsiveNav() {
     $("#nav").toggleClass("responsive")
+}
+
+function setColourScheme() {
+    let theme = "dark"
+    
+    if (localStorage.getItem("theme")) {
+        theme = localStorage.getItem("theme")
+    }else if (window.matchMedia) {
+        if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+            theme = "light"
+        }
+    }
+
+    if (theme == "light") {
+        document.documentElement.setAttribute("data-theme", "light")
+    }
+    updateThemeButton(theme)
+}
+
+function updateThemeButton(theme) {
+    $("#toggle-theme").html(`<i class="fa fa-${theme == "dark" ? "sun" : "moon"}-o"></i>`)
 }
