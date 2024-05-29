86046e91f2d9bd53f4a4b10d033cd4082180d0f2
MattMX <mangrovethefox@outlook.com>
2024-05-29 01:32:54 +0100

    looking good!

--- main.js
+++ main.js
@@ -5,11 +5,26 @@ $(() => {
 
     setTimeout(() => {
         $("#loading-overlay")
-            .css({ 
+            .css({
                 "opacity": 0,
                 "pointer-events": "none"
-             })
+            })
     }, 4)
+
+    fetch("./projects.json").then((data) => data.json())
+        .then((json) => {
+            for (let project of json) {
+                $("#projects").append(`
+                    <a class="project ${project.class}" href="/project?id=${project.id}">
+                        <div class="thumb">
+                            <img class="thumb" src="${project.thumb || "/assets/thumb.png"}">
+                        </div>
+
+                        <h2>${project.name}</h2>
+                    </a>
+                `)
+            }
+        })
 })
 
 function refreshSections() {
